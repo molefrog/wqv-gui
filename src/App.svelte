6911003684a68a6d76d332692e7fa220d903953e
Alexey Taktarov <molefrog@gmail.com>
2024-11-11 09:49:21 +0100

    Custom titlebar

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,7 @@
   import { WatchDevice, getSerialPorts } from "./device.svelte";
   import Photo from "./Photo.svelte";
   import ico_faxWarn from "./icons/fax_warning.png";
+  import Titlebar from "./Titlebar.svelte";
 
   let serialPorts = $state<string[]>([]);
   let device = $state<WatchDevice>();
@@ -35,66 +36,72 @@
   }
 </script>
 
-<main>
-  <div class="window window-main" style="width: 300px">
-    <div class="title-bar">
-      <div class="title-bar-text">Casio WQV dumper</div>
-      <div class="title-bar-controls">
-        <button aria-label="Minimize"></button>
-        <button aria-label="Maximize"></button>
-        <button aria-label="Close"></button>
-      </div>
-    </div>
+<div class="app-win window window-main">
+  <Titlebar />
 
-    <div class="window-body">
-      {#if !device}
-        {#if serialPorts.length === 0}
-          <p class="connect-device">
-            <img src={ico_faxWarn} alt="Warning" width="24" height="24" />
-            Please connect the IR adapter to the computer...
-          </p>
-        {:else}
-          <div class="port-select">
-            <select bind:value={selectedPort}>
-              {#each serialPorts as port}
-                <option value={port}>{port}</option>
-              {/each}
-            </select>
-          </div>
-
-          <button onclick={download}>Download image</button>
-        {/if}
+  <div class="app-win-body window-body">
+    {#if !device}
+      {#if serialPorts.length === 0}
+        <p class="connect-device">
+          <img src={ico_faxWarn} alt="Warning" width="24" height="24" />
+          Please connect the IR adapter to the computer...
+        </p>
       {:else}
-        <Photo {device} onCancel={() => (device = undefined)} />
-      {/if}
-    </div>
+        <div class="port-select">
+          <select bind:value={selectedPort}>
+            {#each serialPorts as port}
+              <option value={port}>{port}</option>
+            {/each}
+          </select>
+        </div>
 
-    <!-- global app status bar -->
-    <div class="status-bar">
-      <p class="status-bar-field">
-        {#if device && device.status === "ready"}
-          Ready
-        {:else if device && device.status === "downloading"}
-          Downloading...
-        {:else if !device && serialPorts.length}
-          Select serial port
-        {:else}
-          Connect device!
-        {/if}
-      </p>
-      <p class="status-bar-field">
-        {#if device && device.status === "downloading"}
-          {device.packetsReceived} PKT
-          {#if device.imgBytesReceived}
-            - {device.imgBytesReceived}B / 7229B
-          {/if}
-        {/if}
-      </p>
-    </div>
+        <button onclick={download}>Download image</button>
+      {/if}
+    {:else}
+      <Photo {device} onCancel={() => (device = undefined)} />
+    {/if}
   </div>
-</main>
+
+  <!-- global app status bar -->
+  <div class="app-win-status status-bar">
+    <p class="status-bar-field">
+      {#if device && device.status === "ready"}
+        Ready
+      {:else if device && device.status === "downloading"}
+        Downloading...
+      {:else if !device && serialPorts.length}
+        Select serial port
+      {:else}
+        Connect device!
+      {/if}
+    </p>
+    <p class="status-bar-field">
+      {#if device && device.status === "downloading"}
+        {device.packetsReceived} PKT
+        {#if device.imgBytesReceived}
+          - {device.imgBytesReceived}B / 7229B
+        {/if}
+      {/if}
+    </p>
+  </div>
+</div>
 
 <style>
+  .app-win {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .app-win-body {
+    flex: 1 1 auto;
+    overflow-y: auto;
+  }
+
+  .app-win-status {
+    flex-shrink: 0;
+  }
+
   .port-select {
     margin-bottom: 4px;
   }
---
+++ src/Titlebar.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import { getCurrentWindow } from "@tauri-apps/api/window";
+
+  // when using `"withGlobalTauri": true`, you may use
+  // const { getCurrentWindow } = window.__TAURI__.window;
+
+  const appWindow = getCurrentWindow();
+</script>
+
+<div class="sv-title title-bar" data-tauri-drag-region>
+  <div class="sv-text title-bar-text">Casio WQV dumper</div>
+  <div class="title-bar-controls">
+    <button aria-label="Minimize" on:click={() => appWindow.minimize()}></button>
+    <button aria-label="Close" on:click={() => appWindow.close()}></button>
+  </div>
+</div>
+
+<style>
+  .sv-title {
+    flex-shrink: 0;
+    user-select: none;
+  }
+
+  .sv-text {
+    pointer-events: none;
+  }
+</style>
